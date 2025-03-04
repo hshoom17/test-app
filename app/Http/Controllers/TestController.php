4ade8d6e14eae690253b0a1bdce5cdc4175844b1
hshoom17 <hashemalahdal17@gmail.com>
2025-03-04 15:29:28 +0300

    x

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function firstAction(){
+
+       ;
+       return view(view: 'welcome');
+       
+    }
+
+    public function greet(){
+
+
+        return 'hello this is greet func';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TestController::class,'firstAction'] 
+);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [TestController::class,'greet'],
+);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+
+
